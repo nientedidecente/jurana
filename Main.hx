c8fccfdf9000118d8c639a4cb3d5e651260ac7f2
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-06-17 22:40:52 +0100

    - added more enemies - renamed main player var

--- Main.hx
+++ Main.hx
@@ -8,27 +8,31 @@ class Main extends App {
 		new Main();
 	}
 
-	var b:Bitmap;
-	var enemy:Bitmap;
-	var velocity = 0;
-
 	static final SPEED = 10;
 	static final ENEMY_SPEED = 10;
 
-	override function init() {
-		b = new Bitmap(Tile.fromColor(0x00ff00, 60, 60), s2d);
-		b.x = 60;
-		b.y = s2d.height * .5;
-		b.tile = b.tile.center();
+	var player:Bitmap;
+	var enemies = new Array<Bitmap>();
+	var velocity = 0;
 
-		enemy = new Bitmap(Tile.fromColor(0xff0000, 60, 60), s2d);
-		enemy.tile = enemy.tile.center();
-		enemy.x = 300;
-		enemy.y = s2d.height * .5;
+	override function init() {
+		player = new Bitmap(Tile.fromColor(0x00ff00, 60, 60), s2d);
+		player.x = 60;
+		player.y = s2d.height * .5;
+		player.tile = player.tile.center();
+
+		for (i in 0...4) {
+			var enemy = new Bitmap(Tile.fromColor(0xff0000, 60, 60), s2d);
+			enemy.tile = enemy.tile.center();
+			enemy.y = s2d.height * .5;
+			enemy.x = 200 + (i * 200);
+
+			enemies.push(enemy);
+		}
 
 		var i = new Interactive(s2d.width, s2d.height, s2d);
 		i.onPush = function(e) {
-			var direction = e.relX <= b.x ? -1 : 1;
+			var direction = e.relX <= player.x ? -1 : 1;
 			velocity = direction * SPEED;
 		}
 
@@ -38,9 +42,11 @@ class Main extends App {
 	}
 
 	override function update(dt:Float) {
-		b.rotation += .05;
-		b.x += velocity;
+		player.rotation += .03;
+		player.x += velocity;
 
-		enemy.rotation += .1;
+		for (enemy in enemies) {
+			enemy.rotation += .01;
+		}
 	}
 }
