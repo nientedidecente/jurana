048e4aafb9d04a24e0b1eb9d2296ddc89e8afeea
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-06-20 15:12:40 +0100

    added small tests (not working unit got deprecated)

---
+++ src/tests/TestRunner.hx
@@ -0,0 +1,13 @@
+package tests;
+
+import tests.libs.RandomizerTestCase;
+
+class TestRunner {
+	static function main() {
+        /*
+		var r = new haxe.unit.TestRunner();
+		r.add(new RandomizerTestCase());
+        r.run();
+        */
+	}
+}
---
+++ src/tests/libs/RandomizerTestCase.hx
@@ -0,0 +1,10 @@
+package tests.libs;
+
+// https://community.haxe.org/t/haxe-unit-and-std-library-deprecation-process/488
+// :(
+class RandomizerTestCase { // extends haxe.unit.TestCase {
+	public function testBasic() {
+		//assertEquals("A", "A");
+	}
+}
+1
